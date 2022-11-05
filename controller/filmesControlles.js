865d278389b3eb3016948982ebe7a2e2453e3044
josi43 <mateusseabra@hotmail.com>
2022-11-05 11:33:41 -0300

    commit atualizando o delete

--- controller/filmesControlles.js
+++ controller/filmesControlles.js
@@ -1,64 +1,51 @@
 const Filme = require("../model/filmeModel");
 const { ObjectID } = require("bson");
 
-async function listar(req,res){
-    await Filme
-    .find({})
-    .then((filmes)=>{
-        return res.json(filmes);
-    })
-    .catch((e)=>{
-        return res.status(500).json(e)
-    })
-}
-
-async function criar(req,res){
-    const filme = new Filme(req.body);
-    await filme
-    .save()
-    .then((doc)=>{
-        return res.status(201).json(doc)
-    })
-    .catch((e)=>{
-        const msgErro = {};
-        Object.values(e.errors)
-        .forEach(({properties}) => {
-            msgErro[properties.path] = properties.message;
-        });
-        return res.status(422).json(msgErro)
-    })
-};
-
-async function deletar(req,res){
-    await Filme.findByIdAndDelete({_id:ObjectID(req.params.id)})
-    .then((filme) => {
-        if(filme) return res.status(204).end();
-        else return res.staus(404).json("Filme nao localizado")
+async function listar(req, res) {
+  await Filme.find({})
+    .then((filmes) => {
+      return res.json(filmes);
     })
     .catch((e) => {
-        return res.status(500).json(e)
+      return res.status(500).json(e);
+    });
+}
+
+async function criar(req, res) {
+  const filme = new Filme(req.body);
+  await filme
+    .save()
+    .then((doc) => {
+      return res.status(201).json(doc);
     })
-}
-async function deletar1(req,res){
-    try{
-        const filme = await Filme.findByIdAndDelete(req.params.id);
-        if(!filme) res.status(404).send("Nao achei o item");
-        res.status(200).send();
-    }catch(e){
-        res.status(500).send(e);
-    }
+    .catch((e) => {
+      const msgErro = {};
+      Object.values(e.errors).forEach(({ properties }) => {
+        msgErro[properties.path] = properties.message;
+      });
+      return res.status(422).json(msgErro);
+    });
 }
 
-async function atualizar(req,res){
-    const filme = new Filme(req.body)
-    try{
-        await Filme.findByIdAndUpdate(req.params.id,req.body);
-        await Filme.save();
-        res.send(filme)
-    }catch(e){
-        res.status(500).send(e)
-    }
+async function deletar(req, res) {
+  try {
+    const filme = await Filme.findByIdAndDelete(req.params.id);
+    if (!filme) res.status(404).send("Nao achei o item");
+    res.status(200).send();
+  } catch (e) {
+    res.status(500).send(e);
+  }
 }
 
+async function atualizar(req, res) {
+  const filme = new Filme(req.body);
+  try {
+    await Filme.findByIdAndUpdate(req.params.id, req.body);
+    await Filme.save();
+    res.send(filme);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+}
 
-module.exports = { listar, criar, deletar, deletar1, atualizar };
+module.exports = { listar, criar, deletar, atualizar };
--- router/filmesRouter.js
+++ router/filmesRouter.js
@@ -1,10 +1,10 @@
-const express = require('express')
+const express = require("express");
 const router = express();
-const controller = require('../controller/filmesControlles')
+const controller = require("../controller/filmesControlles");
 
-router.get('/', controller.listar)
-router.post('/criar', controller.criar)
-router.delete('/deletar/:id', controller.deletar1)
-router.put('/:id', controller.atualizar);
+router.get("/", controller.listar);
+router.post("/criar", controller.criar);
+router.delete("/deletar/:id", controller.deletar);
+router.put("/atualizar/:id", controller.atualizar);
 
-module.exports = router
+module.exports = router;
